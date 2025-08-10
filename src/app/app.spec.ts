09885587202172cd80cfbdfb02ccebcc08d9680a
unknown <yuvrajsune02@gmail.com>
2025-08-10 12:27:30 +0530

    added support of third party tools

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -6,7 +6,7 @@ describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideZonelessChangeDetection()]
+      providers: [provideZonelessChangeDetection()],
     }).compileComponents();
   });
 
@@ -20,6 +20,8 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, portfolio');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, portfolio',
+    );
   });
 });
