033b9897c3947661e81556764e37f6070c026754
unknown <yuvrajsune02@gmail.com>
2025-08-10 12:49:44 +0530

    added main components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Footer } from "./footer/footer";
+import { Hero } from "./hero/hero";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Hero],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
